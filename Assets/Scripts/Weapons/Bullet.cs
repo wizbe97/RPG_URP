70cb8f1b483e96caf0afc7567dbbd4f29f1c728f
Nathan Blore <nathanblore97@gmail.com>
2024-03-25 01:12:44 +0000

    Testing change

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -1,19 +1,26 @@
-using System.Collections;
+using Unity.IO.LowLevel.Unsafe;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
     public float maxTravelDistance;
-    public float destroyBulletDelay;
     public int minimumBulletDamage;
     public int maximumBulletDamage;
 
-    protected Vector3 spawnPosition; // Position where the bullet was instantiated
+    private Vector3 spawnPosition; // Position where the bullet was instantiated
 
     protected virtual void Start()
     {
         spawnPosition = transform.position;
-        StartCoroutine(DestroyAfterDelay(destroyBulletDelay)); // Start coroutine to destroy the bullet after delay
+    }
+
+    private void Update()
+    {
+        float travelDistance = Vector3.Distance(spawnPosition, transform.position);
+        if (travelDistance >= maxTravelDistance)
+        {
+            Destroy(gameObject);
+        }
     }
 
     protected virtual void OnCollisionEnter2D(Collision2D collision)
@@ -27,11 +34,5 @@ public class Bullet : MonoBehaviour
         float t = Mathf.Clamp01(travelDistance / maxTravelDistance);
         int damage = Mathf.RoundToInt(Mathf.Lerp(maximumBulletDamage, minimumBulletDamage, t));
         return damage;
-    }
-
-    protected virtual IEnumerator DestroyAfterDelay(float delay)
-    {
-        yield return new WaitForSeconds(delay);
-        Destroy(gameObject);
     }
 }
