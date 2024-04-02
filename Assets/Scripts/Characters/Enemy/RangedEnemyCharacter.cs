11ececcc521e31df7e892d8b2dda73dfc51bfacf
Nathan Blore <nathanblore97@gmail.com>
2024-04-03 00:17:43 +0100

    Fixing player/enemy collisions. Also sorting sprite index custom axis 0 1 0

--- Assets/Scripts/Characters/Enemy/RangedEnemyCharacter.cs
+++ Assets/Scripts/Characters/Enemy/RangedEnemyCharacter.cs
@@ -54,34 +54,6 @@ public class RangedEnemyCharacter : Character
         }
     }
 
-
-    // Damage Player
-    void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            Player player = collision.gameObject.GetComponent<Player>();
-
-            // Only call DamageCharacter on the player if we don't currently have a DamageCharacter() Coroutine running.
-            if (damageCoroutine == null)
-            {
-                damageCoroutine = StartCoroutine(player.DamageCharacter(damageStrength, 1.0f));
-            }
-        }
-    }
-
-    void OnCollisionExit2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
-        {
-            if (damageCoroutine != null)
-            {
-                StopCoroutine(damageCoroutine);
-                damageCoroutine = null;
-            }
-        }
-    }
-
     // Kill Character (Enemy)
     public override void KillCharacter()
     {
