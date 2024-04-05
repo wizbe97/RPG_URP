2821f997de80f3f9bbf3f387ea98dafc8bdaa3a7
Nathan Blore <nathanblore97@gmail.com>
2024-04-05 01:08:19 +0100

    Revert "Sniper created"
    This reverts commit d4d8d01454590c61675f5723a989522e859ccb3e.

--- Assets/Scripts/ScriptableObjects/HitPoints.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "HitPoints")]
-public class HitPoints : ScriptableObject
-{
-    public float value;
-}
