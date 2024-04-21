e0295e74a05079b4b3d24368ba889886983f7cc3
Nathan Blore <nathanblore97@gmail.com>
2024-04-21 03:56:40 +0100

    Spawnpoints setup

--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager Instance;
+    private Vector3 spawnPosition;
     private void Awake()
     {
         if (Instance == null)
@@ -16,6 +18,32 @@ public class PlayerManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    public void SetSpawnPoint(Vector3 position)
+    {
+        spawnPosition = position;
+    }
+
+    // Method to respawn the player at the spawn point position
+    public void RespawnPlayer()
+    {
+        transform.position = spawnPosition;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        RespawnPlayer();
     }
 
 }
